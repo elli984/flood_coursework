6e2c77742044a3946340ba335ae099797310ac6b
dz350 <123944288+dz350@users.noreply.github.com>
2023-02-08 14:10:17 +0000

    update

--- test_tasks.py
+++ test_tasks.py
@@ -4,22 +4,23 @@ from floodsystem.geo import stations_within_radius
 from floodsystem.geo import rivers_with_station
 from floodsystem.geo import stations_by_river
 from floodsystem.geo import rivers_by_station_number
-
+from floodsystem.station import inconsistent_typical_range_stations
 
 
 
 
 #1B
-# Build list of stations
-stations = build_station_list()
-p = (52.2053, 0.1218) #coordinates of Cambridge city centre
+def test_1b():
+    # Build list of stations
+    stations = build_station_list()
+    p = (52.2053, 0.1218) #coordinates of Cambridge city centre
 
-x = stations_by_distance(stations,p)
+    x = stations_by_distance(stations,p)
 
-lst = []
-for item in x:
-    lst.append(item[2]) 
-assert sorted(lst) == lst
+    lst = []
+    for item in x:
+        lst.append(item[2]) 
+    assert sorted(lst) == lst
 
 
 #1C
@@ -50,30 +51,27 @@ def test_1c():
 
 
 #1D
-from floodsystem.stationdata import build_station_list
+def test_1d():
 
+    stations = build_station_list()
 
-# Build list of stations
-stations = build_station_list()
+    d = rivers_with_station(stations)
 
-d = rivers_with_station(stations)
-
-assert d == ['Addlestone Bourne', 'Aire Washlands', 'Alconbury Brook', 'Aldingbourne Rife', 'Aller Brook', 'Allison Dyke', 'Alverthorpe Beck', 'Ampney Brook', 'Amwell Loop', 'Arkle Beck']
-
+    assert d[0] == 'Addlestone Bourne'
     
 
 
-y = stations_by_river(stations)
-yRA = sorted(y['River Aire'])
-yRC = sorted(y['River Cam'])
-yRT = sorted(y['River Thames'])
+    y = stations_by_river(stations)
+    yRA = sorted(y['River Aire'])
+    yRC = sorted(y['River Cam'])
+    yRT = sorted(y['River Thames'])
 
-assert yRA[0] == 'Airmyn'
-assert yRC[0] == 'Cam'
-assert yRT[0] == 'Abingdon Lock'
+    assert yRA[0] == 'Airmyn'
+    assert yRC[0] == 'Cam'
+    assert yRT[0] == 'Abingdon Lock'
 
 #1E
-def test_1f():
+def test_1e():
     stations = build_station_list()
     top9 = rivers_by_station_number(stations, 9)
     assert top9 == [('River Thames', 55), ('River Avon', 32), ('River Great Ouse', 27),
@@ -83,26 +81,24 @@ def test_1f():
 
 
 #1F
-from floodsystem.stationdata import build_station_list
-from floodsystem.station import inconsistent_typical_range_stations
+def test_1f():
+    
+    stations = build_station_list()
 
-# Build list of stations
-stations = build_station_list()
+    a = inconsistent_typical_range_stations(stations)
+    lst = []
+    for item in a:
+        for item2 in stations:
+            if item2.name == item:
+                lst.append(item2.typical_range_consistent())
+            else:
+                pass
 
-a = inconsistent_typical_range_stations(stations)
-lst = []
-for item in a:
-    for item2 in stations:
-        if item2.name == item:
-            lst.append(item2.typical_range_consistent())
+    counter = 0
+    for aa in lst:
+        if aa == False:
+            counter += 1
         else:
             pass
-
-counter = 0
-for aa in lst:
-    if aa == False:
-        counter += 1
-    else:
-        pass
-assert counter == len(a)
+    assert counter == len(a)
     
